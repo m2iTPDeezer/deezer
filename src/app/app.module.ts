9107b6d8fd7d78b47c9b862be2512f2ecdf21673
Sam <samira.boudiaf59@gmail.com>
2019-09-02 16:21:20 +0200

    ajout du dataservice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { LoginComponent } from './login/login.component';
+import { DataService } from './data.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
-    LoginComponent
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
