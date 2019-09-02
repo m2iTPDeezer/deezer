6fdc77ac542095f15a6035527e52d18277e49343
PusaiKozu <pusaikozu@gmail.com>
2019-09-02 16:18:55 +0200

    Version 1.0 jour 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
-    LoginComponent
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [DataService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
