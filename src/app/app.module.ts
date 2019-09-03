22af4e0307446cb0a47c243ced75ef867f6b1ea9
PusaiKozu <pusaikozu@gmail.com>
2019-09-03 09:29:38 +0200

    Ajout de tous les composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,30 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { AlbumsComponent } from './albums/albums.component';
 import { DataService } from './data.service';
+import { SearchComponent } from './search/search.component';
+import { TracksComponent } from './tracks/tracks.component';
+import { SigninComponent } from './signin/signin.component';
+import { LoginComponent } from './login/login.component';
+import { UseritemmenuComponent } from './useritemmenu/useritemmenu.component';
+import { ItemmenuComponent } from './itemmenu/itemmenu.component';
+import { LikesComponent } from './likes/likes.component';
+import { PlaylistComponent } from './playlist/playlist.component';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    AlbumsComponent
+    AlbumsComponent,
+    SearchComponent,
+    TracksComponent,
+    SigninComponent,
+    LoginComponent,
+    UseritemmenuComponent,
+    ItemmenuComponent,
+    LikesComponent,
+    PlaylistComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
