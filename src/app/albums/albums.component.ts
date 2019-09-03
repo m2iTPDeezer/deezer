392a2a49586c524421ea313a1af23b01c02781b1
Sam <samira.boudiaf59@gmail.com>
2019-09-03 15:28:49 +0200

    remplacement des liens par des balises p dans fichier albums.html

--- src/app/albums/albums.component.ts
+++ src/app/albums/albums.component.ts
@@ -7,13 +7,29 @@ import { DataService } from '../data.service';
   styleUrls: ['./albums.component.css']
 })
 export class AlbumsComponent implements OnInit {
-album;
+  album;
+  id;
+  tab: Array<any> = new Array<any>();
 
   constructor(private data: DataService) { }
 
   ngOnInit() {
 
     this.album = this.data.albums;
+
+  }
+
+  like = () => {
+    console.dir(this.album)
+    // if (this.id != 0) {
+
+    //   let elt = this.album.find((u => u.id == this.album.id));
+
+    //   this.tab.push(this.album.id);
+    //   console.log(this.tab)
+
+    // }
+
   }
 
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,24 +7,28 @@ export class DataService {
 
   albums = [
     {
+      id: 1,
       title: "Nssi, Nssi",
       chanteur: "Khaled",
       image:"assets/album1.jpg",
       style: "#RAÏ"
     },
     {
+      id: 2,
       title: "You're the one i need",
       chanteur: "Barry White",
       image: "assets/album2.jpg",
       style: "#pop"
     },
     {
+      id: 3,
       title: "I believe i can fly",
       chanteur: "R Kelly",
       image: "assets/album3.jpg",
       style: "#magic song"
     },
     {
+      id: 4,
       title: "Aaja Nachle ",
       chanteur: "Sunidhi Chauhan",
       image: "assets/album4.jpg",
